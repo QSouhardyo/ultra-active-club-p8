1d1e55c05c436898293c13272bcea58214a8c022
QSouhardyo <quaziaumio98@gmail.com>
2022-09-30 22:28:11 +0600

    last

--- src/component/Question/Question.js
+++ src/component/Question/Question.js
@@ -44,7 +44,7 @@ const Question = () => {
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="headingThree">
                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-
+                            What's the work of useEffect Without data Loading?
                         </button>
                     </h2>
                     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
